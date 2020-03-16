0a7d34be3fe0d5972bf2e98c0f1809fbe8921133
Niklas Forsström <niklas@r-36-99-25-172.comp.nus.edu.sg>
2020-03-16 13:07:39 +0800

    added a Main file ad updated gitignore

---
+++ Main.py
@@ -0,0 +1,4 @@
+import PartialEvaluator as pe
+
+program = "hello; my name is Peter; I am 26 years old"
+pe.partially_evaluate(program);
--- PartialEvaluator.py
+++ PartialEvaluator.py
@@ -1,17 +1,8 @@
 #use semicolons to differnetiate "lines"
 
-#def partially_evaluate(program):
-#    my_list = []
-#    i = 0;
-#    while (i<program)
-#   return 2
-
 
 def partially_evaluate (program):
     modified = False  #flag that cehecks if we have been able ot improve the program.
     lines = program.split(";");
     for line in lines:
         print(line)
-
-program = "hello; my name is Peter; I am 26 years old"
-partially_evaluate(program);
